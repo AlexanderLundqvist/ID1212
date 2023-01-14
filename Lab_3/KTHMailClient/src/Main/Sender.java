a544c7cd430402215b56ac0377a969d7498478b8
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2023-01-15 00:41:30 +0100

    Update Sender.java

--- Lab_3/KTHMailClient/src/Main/Sender.java
+++ Lab_3/KTHMailClient/src/Main/Sender.java
@@ -3,8 +3,10 @@ package Main;
 import java.io.*;
 import java.io.File;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
 import java.util.Base64;
-import java.util.Map;
+import java.util.Date;
 import java.util.Scanner;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -25,20 +27,13 @@ import javax.net.ssl.SSLSocketFactory;
  *
  * @author Alexander Lundqvist & Ramin Shojaei
  */
-public class Sender {
-    private static final Map<Integer, String> SMTPresponses = Map.of(
-            200, "200 OK",
-            400, "400 Bad Request",
-            404, "404 Not Found",
-            405, "405 Method Not Allowed",
-            500, "500 Internal Server Error",
-            505, "505 HTTP Version Not Supported");
-    
+public class Sender {  
     private static final String SERVER = "smtp.kth.se";
     private static final int PORT = 587;
     private static String username;
     private static String password;
     private static String recipient;
+    private static String subject;
     private static String message;
     private static Socket socket;
     private static BufferedReader incoming;
@@ -55,16 +50,18 @@ public class Sender {
             the user can specify the recipient and the message.
             */
             Scanner file = new Scanner(new File("src/resources/Credentials.txt"));
-            username = file.nextLine().trim();
-            password = file.nextLine().trim();
+            username = file.nextLine();
+            password = file.nextLine();
             file.close();
             System.out.println("Login credentials has been processed.");
 
             Scanner in = new Scanner(System.in);
             System.out.println("Recipient: ");
-            recipient = in.nextLine().trim();
+            recipient = in.nextLine();
+            System.out.println("Subject: ");
+            subject = in.nextLine();
             System.out.println("Message: ");
-            message = in.nextLine() + "\r\n.\r\n"; // <CRLF>.<CRLF> to end message 
+            message = in.nextLine() + "\r\n."; // <CRLF>. to end message 
             in.close();
             System.out.println();
             
@@ -74,7 +71,8 @@ public class Sender {
             socket = new Socket(SERVER, PORT);
             incoming = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             outgoing = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            System.out.println(incoming.readLine()); // Initialize the connection by reading the first line
+            System.out.println(getTimestamp() + " Starting SMTP mail service...");
+            System.out.println(getTimestamp() + " S: " + incoming.readLine()); // Initialize the connection by reading the first line
             
             /*
             Greet the server through the connection, specifying that
@@ -102,20 +100,19 @@ public class Sender {
             works over the SSL socket. Then we send it to the server for authentication.
             */
             Base64.Encoder encoder = Base64.getEncoder();
-            String usernameEncode = encoder.encodeToString(username.getBytes());
-            String passwordEncode = encoder.encodeToString(password.getBytes());
-            sendMessage(usernameEncode, false);
-            sendMessage(passwordEncode, false);
+            sendMessage(encoder.encodeToString(username.getBytes()), false);
+            sendMessage(encoder.encodeToString(password.getBytes()), false);
 
             /*
             After successful authentication we can start to send the actual message.
             */
-            sendMessage("MAIL FROM:<" + username + "@kth.se>", true);
-            sendMessage("RCPT TO:<" + recipient + "@kth.se>", true);
-            sendMessage("DATA", true);
-            sendMessage(message, true);
-            sendMessage("QUIT", true);
-
+            sendMessage("MAIL FROM:<" + username + "@kth.se>", false);
+            sendMessage("RCPT TO:<" + recipient + "@kth.se>", false);
+            sendMessage("DATA", false);
+            sendMessage(getHeaders()+message, false);
+            sendMessage("QUIT", false);
+            
+            System.out.println(getTimestamp() + " SMTP mail service has stopped!");
             sslSocket.close();
             socket.close();
         } catch (Exception exception) {
@@ -127,7 +124,7 @@ public class Sender {
     /**
      * Sends a message to the mail server. 
      * @param message the message to be sent
-     * @param read specifies if we want to read the response
+     * @param read specifies if we want to read a specific response
      */
     private static void sendMessage(String message, boolean read) {
         try {
@@ -137,7 +134,8 @@ public class Sender {
                 readResponse(message);
             }
             else {
-                System.out.println(incoming.readLine());
+                System.out.println(getTimestamp() + " C: " + message);
+                System.out.println(getTimestamp() + " S: " + incoming.readLine());
             }
         } catch (Exception exception) {
             System.err.println("Error sending message: " + exception.getMessage());
@@ -145,23 +143,63 @@ public class Sender {
     }
     
     /**
-     * Reads the response from the server.
+     * Reads the response from the server and prints the message from both client
+     * and server.
      */
     private static void readResponse(String message) {
         try {
             String line;
-            System.out.println();
-            System.out.println(message);
-            while ((line = incoming.readLine()) != null) { 
-                //System.out.println(line); 
-                if (line.startsWith("220 ") || line.startsWith("235 ") || line.startsWith("250 ") || line.startsWith("334 ") || line.startsWith("354 ")) {
-                    System.out.println(line);
-                    break;
+            System.out.println(getTimestamp() + " C: " + message);
+            while ((line = incoming.readLine()) != null) {
+                if (message.startsWith("EHLO") || message.startsWith("HELO")) {
+                    // https://datatracker.ietf.org/doc/html/rfc1830
+                    // Uncomment to show the whole EHLO response. 
+                    //System.out.println(getTimestamp() + " S: " + line); 
+                    if (line.startsWith("250 ")) {
+                        System.out.println(getTimestamp() + " S: " + line);
+                        break;
+                    }
+                }
+                else if (message.startsWith("STARTTLS")) {
+                    if (line.startsWith("220 ")) {
+                        System.out.println(getTimestamp() + " S: " + line);
+                        break;
+                    }
+                }
+                else if (message.startsWith("AUTH LOGIN")) {
+                    if (line.startsWith("235 ") || line.startsWith("334 ")) {
+                        System.out.println(getTimestamp() + " S: " + line);
+                        break;
+                    }
                 }
             }
         } catch (IOException exception) {
             System.err.println("Error reading server response: " + exception.getMessage());
         }
+    }
+    
+    /**
+     * Gets the email headers.
+     * @return the headers
+     */
+    private static String getHeaders() {
+        String headers =
+            "Date: " + LocalTime.now() + "\r\n" +
+            "From: SMTP User <" + username + "@kth.se>\r\n" +
+            "Subject: " + subject + "\r\n" +
+            "To: " + recipient + "@kth.se\r\n\r\n";
+            
+        return headers;
+    }
+    
+    /**
+     * A simple function to create a timestamp.
+     * @return the timestamp 
+     */
+    private static String getTimestamp() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String timeStamp = "[" + formatter.format(new Date()) + "]";
+        return timeStamp;
     }
     
 }
