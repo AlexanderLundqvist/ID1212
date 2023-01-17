af73e4e5ba37203ca1bceb442e4ec221f33b4505
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2023-01-17 18:19:26 +0100

    Update Receiver.java

--- Lab_3/KTHMailClient/src/Main/Receiver.java
+++ Lab_3/KTHMailClient/src/Main/Receiver.java
@@ -3,6 +3,8 @@ package Main;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -21,26 +23,26 @@ import javax.net.ssl.SSLSocketFactory;
  * @author Alexander Lundqvist & Ramin Shojaei
  */
 public class Receiver {
-
+    private static final String SERVER = "webmail.kth.se";
+    private static final int PORT = 993;
+    private static final String inbox = "a02 SELECT INBOX"; // inkorg a002
+    private static final String firstMail = "a03 FETCH 1 BODY[TEXT]"; // Hämtar äldsta e-postmeddelandet i inkorgen genom detta kommando.
+    private static final String logout = "999 LOGOUT";
+    private static String username;
+    private static String password;
+    private static SSLSocket sslSocket;
+    private static SSLSocketFactory socketFactory;
+    private static BufferedReader incoming;
+    private static PrintWriter outgoing;
+    private static String login;
+    
+    
     /**
      * Main method.
      *
      * @param args takes no input arguments
      */
     public static void main(String[] args) throws IOException {
-        final String SERVER = "webmail.kth.se";
-        final int PORT = 993;
-        String username = null;
-        String password = null;
-        SSLSocket sslSocket = null;
-        SSLSocketFactory socketFactory = null;
-        BufferedReader incoming = null;
-        PrintWriter outgoing = null;
-        String login = null;
-        final String inbox = "a02 SELECT INBOX"; // inkorg a002
-        final String firstMail = "a03 FETCH 1 BODY[TEXT]"; // Hämtar äldsta e-postmeddelandet i inkorgen genom detta kommando.
-        final String logout = "999 LOGOUT";
-
         try {
             /* 
             We get the login credentials by reading it from a secret file.
@@ -62,48 +64,73 @@ public class Receiver {
 
             // Referens till detta: https://www.rfc-editor.org/rfc/rfc3501#section-6.4.5
             // Läser in hälsning från servern, från sslSocket
-            System.out.println("S: " + incoming.readLine());  //Response från Server
-
-            messageWriter(outgoing, login);
-            messageReader(incoming);
-
-            messageWriter(outgoing, inbox);
-            messageReader(incoming);
-
-            messageWriter(outgoing, firstMail);
-            messageReader(incoming);
-
-            messageWriter(outgoing, logout);
-            messageReader(incoming);        
-
-        } catch (Exception e) {
-            System.err.println(e);
-            e.printStackTrace();
+            System.out.println(getTimestamp() + " S: " + incoming.readLine());  //Response från Server
+            
+            /*
+            Finally specify the mail we want to receive.
+            */
+            sendMessage(login);
+            sendMessage(inbox);
+            sendMessage(firstMail);
+            sendMessage(logout);
+            
+        } catch (Exception exception) {
+            System.err.println("Error: " + exception.getMessage());
+            exception.printStackTrace();
         } finally {
             try {
                 sslSocket.close();
             } catch (IOException exception) {
                 System.err.println("Could'd not close the socket: " + exception.getMessage());
-                //exception.printStackTrace();
             }
         }
     }
-
-    public static void messageWriter(PrintWriter outgoing, String message) throws IOException {
-        outgoing.println(message);
-        outgoing.flush();
-        System.out.println("C: " + message);
-    }
-
-    public static void messageReader(BufferedReader incoming) throws IOException {
-        String message;
-        while ((message = incoming.readLine()) != null) {
-            //System.out.println("S: " + message);
-            System.out.println("S: " + new String (message.getBytes("UTF-8"), System.getProperty("native.encoding"))); // Works for some reason...
-            if (message.contains("a0")) {
-                break;
+    
+    /**
+     * Sends a message to the mail server. 
+     * @param message the message to be sent
+     */
+    public static void sendMessage(String message) {
+        try {
+            outgoing.println(message);
+            outgoing.flush();
+            if (message.startsWith("a01 LOGIN ")) {
+                System.out.println(getTimestamp() + " C: a01 LOGIN " + username + " ************");
             }
+            else {
+                System.out.println(getTimestamp() + " C: " + message);
+            }
+            readResponse();
+        } catch (Exception exception) {
+            System.err.println("Error sending message: " + exception.getMessage());
         }
+    }
+    
+    /**
+     * Reads the response from the server and prints it.
+     */
+    public static void readResponse() {
+        try {
+            String line;
+            while ((line = incoming.readLine()) != null) {
+                System.out.println(getTimestamp() + " S: " + line);
+                if (line.contains("a0")) {
+                    break;
+                }
+            }
+        } catch (IOException exception) {
+            System.err.println("Error reading server response: " + exception.getMessage());
+        }
+    }
+    
+    /**
+     * A simple function to create a timestamp.
+     * @return the timestamp 
+     */
+    private static String getTimestamp() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        String timeStamp = "[" + formatter.format(new Date()) + "]";
+        return timeStamp;
     }
 
 }
